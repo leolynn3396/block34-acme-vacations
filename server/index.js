362c6580d199e8cefbd08e9008c70a9c30b51354
Ling Huang <linghuang@Lings-MacBook-Pro.local>
2024-04-04 18:50:02 -0700

    guided practice

---
+++ server/index.js
@@ -0,0 +1,83 @@
+const { client, createTables, createUser, createPlace,  fetchUsers, fetchPlaces,  createVacation, fetchVacations, destroyVacation} = require('./db');
+
+const express = require('express');
+const app = express();
+
+//GET Route
+app.get('/api/users', async(req,res,next) => {
+    try{
+        res.send(await fetchUsers());
+    }catch(ex){
+        next(ex);
+    }
+});
+app.get('/api/places', async(req,res,next) => {
+    try{
+        res.send(await fetchPlaces());
+    }catch(ex){
+        next(ex);
+    }
+});
+app.get('/api/vacations', async(req,res,next) => {
+    try{
+        res.send(await fetchVacations());
+    }catch(ex){
+        next(ex);
+    }
+});
+
+//DELETE Route
+app.delete('/api/vacations/:id', async(req, res, next) => {
+    try{
+        await destroyVacation(req.params.id);
+        res.sendStatus(204);
+    }catch(ex){
+        next(ex);
+    }
+});
+
+//POST Route
+app.post('/api/vacations', async(req, res, next) => {
+    try {
+        res.status(201).send(await createVacation(req.body));
+    }
+    catch(ex){
+        next(ex);
+      }
+});
+
+const init = async()=> {
+    await client.connect();
+    console.log('connected to database');
+    await createTables();
+    console.log('tables created');
+    const [moe, lucy, ethyl, rome, nyc, la, paris] = await Promise.all([
+        createUser('moe'),
+        createUser('lucy'),
+        createUser('ethyl'),
+        createPlace('rome'),
+        createPlace('nyc'),
+        createPlace('la'),
+        createPlace('paris')
+      ]);
+      console.log(`moe has an id of ${moe.id}`);
+      console.log(`rome has an id of ${rome.id}`);
+      console.log(await fetchUsers());
+      console.log(await fetchPlaces());
+      await Promise.all([
+        createVacation({ user_id: moe.id, place_id: nyc.id, departure_date: '04/01/2024'}),
+        createVacation({ user_id: moe.id, place_id: nyc.id, departure_date: '04/15/2024'}),
+        createVacation({ user_id: lucy.id, place_id: la.id, departure_date: '07/04/2024'}),
+        createVacation({ user_id: lucy.id, place_id: rome.id, departure_date: '10/31/2024'}),
+      ]);
+      const vacations = await fetchVacations();
+      console.log(vacations);
+      await destroyVacation(vacations[0].id);
+      console.log(await fetchVacations());    
+
+      const port = process.env.PORT || 3000;
+      app.listen(port, ()=> console.log(`listening on port ${port}`));
+
+  };
+  
+  init();
